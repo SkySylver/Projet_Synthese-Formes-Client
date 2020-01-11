be823efd017f9796dc229bbdd6ab7e4053b77c80
emincan <emin_can@live.fr>
2020-01-11 17:32:11 +0000

    Update FormeComposee.cpp

--- Client/FormeComposee.cpp
+++ Client/FormeComposee.cpp
@@ -1,1 +1,11 @@
 #include "FormeComposee.h"
+
+FormeComposee::FormeComposee(const Forme& f) {
+	vector<Forme>::iterator it = find(_groupes.begin(), _groupes.end(), f);
+	if (it != end(_groupes)) {
+		_groupes.push_back(f);
+	}
+	else {
+		throw invalid_argument("Un groupe contient déjà cette forme");
+	}
+}
