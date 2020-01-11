09a7bb285c7f07d9e44357ef6f1861bb233f1e58
emincan <emin_can@live.fr>
2020-01-11 17:52:45 +0000

    Update Segment.h

--- Client/Segment.h
+++ Client/Segment.h
@@ -8,7 +8,9 @@ private:
 public:
 	Segment(const Vecteur2D &a, const Vecteur2D & b, const string & couleur = "black");
 	~Segment() {};
-
+	
+    double getAire() const { return 0; }
+    
 	Vecteur2D getA() { return _a; };
 	Vecteur2D getB() { return _b; };
 	void setA(Vecteur2D a) { _a = a; };
