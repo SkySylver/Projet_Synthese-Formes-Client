0d8e0b9d332de72de5c070450dc32efb5bbc627d
emincan <emin_can@live.fr>
2020-01-11 17:15:21 +0000

    Update Forme.h

--- Client/Forme.h
+++ Client/Forme.h
@@ -1,19 +1,19 @@
 #pragma once
-#include "Vecteur2D.h"
 #include <string>
+#include <stdexcept>
+
 using namespace std;
 class Forme
 {
 private:
 	string _couleur;
+	string _couleurs[6] = { "black",  "blue", "red", "green", "yellow", "cyan" };
 	
 public:
 
 	Forme() {};
 	~Forme() {};
-	Forme(string couleur):_couleur(couleur) {};
-
-
+	Forme(string couleur);
 
 	void setCouleur(const string &couleur) { _couleur = couleur; };
 	string getCouleur() { return _couleur; };
