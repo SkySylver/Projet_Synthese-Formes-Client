728eff22168ffb7c3119fc3b7d45a6bb4a946f6b
Enes Usta <mine.skysylver@gmail.com>
2020-01-12 17:46:14 +0100

    Polygone fini

--- Client/Polygone.cpp
+++ Client/Polygone.cpp
@@ -1,20 +1,67 @@
 #include "Polygone.h"
 
 
-double getAire() const;
 
-Polygone::operator string() const {
 
+vector<Vecteur2D> Polygone::getVecteurs() {
+	return _vecteurs;
+}
+void Polygone::setVecteur(const vector<Vecteur2D> & v) {
+
+	_vecteurs = v;
 }
 
-Forme * Polygone::Translation(const Vecteur2D & v)const {
+void Polygone::ajouterVecteur(Vecteur2D v) {
+	_vecteurs.push_back(v);
+}
 
+void Polygone::retirerVecteur(Vecteur2D v) {
+	//vector<Vecteur2D>::iterator it = find(_vecteurs.begin(), _vecteurs.end(), v);
+//		_vecteurs.erase(it);
+}
+
+
+Polygone::operator string() const {
+	ostringstream os;
+	os << "(Polygone :";
+	for (vector<Vecteur2D>::const_iterator it = _vecteurs.begin(); it != _vecteurs.end(); ++it) {
+		os << *it << ",";
+	}
+	return os.str();
+}
+
+
+Polygone & Polygone::operator + (const Vecteur2D & v) {
+	ajouterVecteur(v);
+	return *this;
+}
+
+Polygone & Polygone::operator - (const Vecteur2D & v) {
+	retirerVecteur(v);
+	return *this;
+}
+
+
+Forme * Polygone::Translation(const Vecteur2D & v)const {
+	vector<Vecteur2D> vec;
+	for (vector<Vecteur2D>::const_iterator it = _vecteurs.begin(); it != _vecteurs.end(); it++)
+		vec.push_back(it->Translation(v));
+
+	return new Polygone(vec, _couleur);
 }
 
 Forme * Polygone::Homothetie(const Vecteur2D & v, const double rapport)const {
+	vector<Vecteur2D> vec;
+	for (vector<Vecteur2D>::const_iterator it = _vecteurs.begin(); it != _vecteurs.end(); it++)
+		vec.push_back(it->Homothetie(v, rapport));
 
+	return new Polygone(vec, _couleur);
 }
 
 Forme * Polygone::Rotation(const Vecteur2D & v, const double angle) const {
+	vector<Vecteur2D> vec;
+	for (vector<Vecteur2D>::const_iterator it = _vecteurs.begin(); it != _vecteurs.end(); it++)
+		vec.push_back(it->Rotation(v, angle));
 
+	return new Polygone(vec, _couleur);
 }
--- Client/Polygone.h
+++ Client/Polygone.h
@@ -7,11 +7,17 @@
 class Polygone: public Forme
 {
 private :
-	vector<Vecteur2D>_vecteurs;
+	vector<Vecteur2D> _vecteurs;
 public :
 	Polygone(const vector<Vecteur2D> & v, string couleur): Forme(couleur), _vecteurs(v){}
 	~Polygone() {};
 	//virtual bool dessiner(VisiteurDessin visiteur) const = 0;
+
+	vector<Vecteur2D> getVecteurs();
+	void setVecteur(const vector<Vecteur2D> & v);
+
+	void ajouterVecteur(Vecteur2D v);
+	void retirerVecteur(Vecteur2D v);
 
 	double getAire() const;
 
@@ -20,5 +26,9 @@ public :
 	Forme * Translation(const Vecteur2D & v)const;
 	Forme * Homothetie(const Vecteur2D & v, const double rapport)const;
 	Forme * Rotation(const Vecteur2D & v, const double angle) const;
+
+	Polygone & operator + (const Vecteur2D & v);
+	Polygone & operator - (const Vecteur2D & v);
+
 };
 
