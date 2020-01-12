24edcaa19820954a87a65840e68687e1ce5e748f
emincan <emin_can@live.fr>
2020-01-12 21:26:59 +0000

    Upload New File

---
+++ Client/Sauvegarde/Sauvegarde.cpp
@@ -0,0 +1,21 @@
+#include "../Erreur.h"
+#include "Sauvegarde.h"
+
+const string Sauvegarde::Ecriture(ostringstream &nomFichier,ostringstream &texte)const{
+		ofstream file(nomFichier.str());
+		if (file.is_open())
+			file << texte.str();
+		else throw Erreur("Impossible d'ouvrir le fichier");
+		file.close();
+}
+
+const string Sauvegarde::visiteSegment(const Segment *forme) const{
+	ostringstream nomFichier, texte;
+	nomFichier << "segment" << _num << ".txt";
+
+	texte << "Segment;" << forme->getA().x << "," << forme->getA().y << ",";
+	texte << forme->getB().x << "," << forme->getB().y << ",";
+	texte << forme->getCouleur() << "," << endl;
+
+    return Sauvegarde::Ecriture(_sauvegarde,nomFichier, texte);
+}
