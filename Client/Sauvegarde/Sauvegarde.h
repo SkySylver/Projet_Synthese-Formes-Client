f98fae4752e65c25e3ca574306d6a5dc026c6d84
emincan <emin_can@live.fr>
2020-01-12 21:26:35 +0000

    Update Sauvegarde.h

--- Client/Sauvegarde/Sauvegarde.h
+++ Client/Sauvegarde/Sauvegarde.h
@@ -1,27 +1,21 @@
 #pragma once
-#include "../VisiteurSauvegarde.h"
-using namespace std;
+#include "Forme.h"
+#include "Cercle.h"
+#include "Segment.h"
+#include "Triangle.h"
+#include "Polygone.h"
+#include "FormeComposee.h"
 
-class Sauvegarde : public VisiteurSauvegarde{
-	static int _incrementation;
+class VisiteurSauvegarde
+{
 public:
-    /**Constructeur
-	 * @param envoi de type bool : Verifie et indique si la chaine d�sirant �tre sauvegard� est a envoyer*/
-	Sauvegarde(bool envoi = true) : VisiteurSauvegarde(envoi)  {
-	    _incrementation = _incrementation + 1;
-    }
-    /**Evite la redondance de code dans les visiteurs, retourne un string et �crit dans un fichier*/
-    const string Ecriture(bool sauvegarde,ostringstream &nomFichier,ostringstream &texte) const;
-    /** Sauvegarde un cercle
-	 * @param Forme de type cercle.
-	 * @return Nom du fichier ou l'objet sous forme de string si l'envoi est inscrit comme faux*/
-	const string visiteCercle(const Cercle * forme) const;
-	/**M�me d�marche que pour le cercle*/
-	const string visiteSegment(const Segment * forme) const;
-	const string visiteTriangle(const Triangle * forme) const;
-	const string visitePolygone(const Polygone * forme) const;
-	const string visiteFormeComposee(const FormeComposee * forme) const;
-	/** Stocke les valeurs d'une forme selon le type de forme */
-	template<class T>
-	const void StockageTriPoly(ostringstream &nomFichier,ostringstream &texte,const T *forme)const;
+
+	VisiteurSauvegarde(){}
+	virtual void visite(const Segment* f)const = 0;
+	virtual void visite(const Cercle* f)const = 0;
+	virtual void visite(const Triangle* f)const = 0;
+	virtual void visite(const Polygone* f)const = 0;
+	virtual void visite(const FormeComposee* f)const = 0;
+
 };
+
