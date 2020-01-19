bf6b9c9f81f9d0cb382ff9d8a06ad18f8d30262c
Enes Usta <mine.skysylver@gmail.com>
2020-01-19 15:07:31 +0100

    Main tests pas fini

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -2,23 +2,119 @@
 #include "VisiteurServeur.h"
 #include "Connexion.h"
 #include "Erreur.h"
+#include "Polygone.h"
+
 using namespace std;
+
+bool is_number(const string& s)
+{
+	char* end = 0;
+	double val = strtod(s.c_str(), &end);
+	return end != s.c_str() && *end == '\0' && val != HUGE_VAL;
+}
+
+Vecteur2D saisirPoint(string n) {
+
+	string x, y;
+	double x1, y1;
+	char * end = 0;
+	do {
+		cout << "Veuillez saisir x" << n << endl;
+		cin >> x;
+
+		cout << "Veuillez saisir y" << n << endl;
+		cin >> y;
+
+	} while (!is_number(x) && !is_number(y));
+	end = 0;
+	x1 = strtod(x.c_str(), &end);
+	end = 0;
+	y1 = strtod(x.c_str(), &end);
+
+	Vecteur2D v(x1, y1);
+	return v;
+
+}
 
 int main()
 {
 
 	WSAData _wsaData;
+	string reponse;
 
 	int _r;
 	_r = WSAStartup(MAKEWORD(2, 0), &_wsaData);       // MAKEWORD(2,0) sert à indiquer la version de la librairie à utiliser : 1 pour winsock et 2 pour winsock2
 
 	if (_r) throw Erreur("L'initialisation a échoué");
+
+
+
 //	initWinsockLib(_wsaData);
 	/*	WSADATA wsaData;
 		int familleAdresse = AF_INET;
 		int typeSocket = SOCK_STREAM;
 		int protocole = IPPROTO_TCP;
 		*/
+
+
+	Forme * forme;
+	string x, y;
+	double xtemp;
+	char * end;
+	vector<Vecteur2D> v;
+	int i = 0;
+	Polygone* p = new Polygone(v);
+
+	while (true) {
+		
+		cout << "Que souhaitez-vous faire :" << endl;
+		cout << "1 : Creer FormeComposee" << endl;
+		cout << "2 : Creer Segment" << endl;
+		cout << "3 : Creer Polygone" << endl;
+		cout << "4 : Creer Cercle" << endl;
+		cout << "5 : Creer Triangle" << endl;
+
+		cout << "6 : Creer Connexion" << endl;
+		cout << "7 : Creer VisiteurServeur" << endl;
+
+		cin >> reponse;
+
+		switch (stoi(reponse)) {
+		case 1:
+			forme = new FormeComposee();
+			break;
+		case 2:
+			forme = new Segment(saisirPoint("1"), saisirPoint("2"));
+			break;
+		case 3:			
+			while(true) {
+				cout << "Voulez-vous ajouter un point au vecteur ? (o/n)";
+				cin >> reponse;
+				
+				if (strcmp(reponse.c_str(), "o")) break;
+
+				p->ajouterVecteur(saisirPoint(to_string(i)));				
+			}
+			forme = p;
+			break;
+		case 4:
+			cin >> reponse;
+			xtemp = stoi(reponse);
+			forme = new Cercle(saisirPoint("Centre"), xtemp);
+
+//			saisirPoint
+
+		}
+
+
+			
+
+
+
+	}
+
+
+
 	char adresseServeur[L] = "10.11.85.253";
 	short portServeur = 6666;
 
