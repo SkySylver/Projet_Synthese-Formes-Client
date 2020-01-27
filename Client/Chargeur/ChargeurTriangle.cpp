cb762bdf741166774877fd49d622c6a21eca9f3f
emincan <emin_can@live.fr>
2020-01-27 10:23:53 +0000

    Update ChargeurTriangle.cpp

--- Client/Chargeur/ChargeurTriangle.cpp
+++ Client/Chargeur/ChargeurTriangle.cpp
@@ -2,6 +2,7 @@
 Triangle * ChargeurTriangle::chargerForme(const string& nomFichier) const {
 	string contenu;
 	ifstream fichier;
+	Triangle * temp;
 
 	fichier.open(nomFichier.c_str(), fstream::in);
 
@@ -13,9 +14,19 @@ Triangle * ChargeurTriangle::chargerForme(const string& nomFichier) const {
 	fichier >> contenu;
 
 	if (contenu.substr(0, 8) == "Triangle"){
-		cout << "triangle charge" << endl;
-		//Charger Triangle
+		stringstream ss(contenu);
+		string token;
+		int i = 0;
+		while (getline(ss, token, ',')) {
+			arr[i] = token;
+			i++;
+		}
+		Vecteur2D a(stod(arr[2]), stod(arr[3]));
+		Vecteur2D b(stod(arr[4]), stod(arr[5]));
+		Vecteur2D c(stod(arr[6]), stod(arr[7]));
 
+		fichier.close();
+		return new Triangle(a, b, c, arr[1]);
 	}
 
 
